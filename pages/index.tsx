8b04e951802a0bf2148fc6a651f0dad85e63c3fc
ErenKarakaya <erenkarakaya@eskisehir.edu.tr>
2022-03-27 15:12:10 +0300

    typescript added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,29 @@
-import Head from "next/head"
-import Layout, { siteTitle } from "../components/layout"
-import utilStyles from "../styles/utils.module.scss"
-import { getSortedPostsData } from "../lib/posts"
-import Link from "next/link"
-import Date from "../components/date"
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-const Home = ({ allPostsData }) => {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi. I'm trying to be a talented fullstack developer.</p>
+        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -38,13 +47,11 @@ const Home = ({ allPostsData }) => {
   )
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const allPostsData = getSortedPostsData()
   return {
     props: {
-      allPostsData,
-    },
+      allPostsData
+    }
   }
 }
-
-export default Home
